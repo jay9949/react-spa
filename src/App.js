b3d05aeb29243252a5b97843fb3875258359e0c3
jay9949 <jay37192@gmail.com>
2023-12-25 11:18:19 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
+// import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+// import Layout from "./components/Layout/Layout";
+// import UserProfile from "./components/Profile/UserProfile";
+// import AuthPage from "./pages/AuthPage";
+// import HomePage from "./pages/HomePage";
+
+// function App() {
+//   return (
+//     <Layout>
+//       <Routes>
+//         <Route path="/" exact>
+//           <HomePage />
+//         </Route>
+//         <Route path="/auth">
+//           <AuthPage />
+//         </Route>
+//         <Route path="/profile">
+//           <UserProfile />
+//         </Route>
+//       </Routes>
+//     </Layout>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/profile" element={<UserProfile />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
